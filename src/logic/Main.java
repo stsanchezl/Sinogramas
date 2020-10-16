42bf939d0c4c76e6da2b338708934d44e7d2039c
stsanchezl <stsanchezl@unal.edu.co>
2020-10-16 17:57:22 -0500

    QueueArray Debug

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,7 +1,10 @@
 package logic;
-
+import data.QueueArrayGeneric;
 public class Main {
     public static void main(String[] args) {
-        Runner.runner();
+        //Runner.runner();
+        QueueArrayGeneric q = new QueueArrayGeneric<>(4);
+        for (int i)
+
     }
 }
