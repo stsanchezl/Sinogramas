dffe5126e0019b2a6bfc7a083a1847f4033676ef
stsanchezl <stsanchezl@unal.edu.co>
2020-10-16 16:05:38 -0500

    Current basic program.

--- src/logic/Runner.java
+++ src/logic/Runner.java
@@ -6,20 +6,46 @@ import ui.CommandLines;
 public class Runner {
 
     public static void runner() {
-
         //My path is = /home/stiven/Documentos/Java/estructurasDeDatos/Unihan/mergedFiles.txt
-        CommandLines.print("Ingrese el camino al archivo .txt (incluyendo el archivo)");
+        CommandLines.welcome();
+        String dataStructure = CommandLines.selectDataStructure();
+        CommandLines.print("Type the path to the file (including the .txt extension).");
         String pathToFile = CommandLines.input();
-        ChineseCharacters letras = new ChineseCharacters(pathToFile, CommandLines.selectArraysOrReferences());
+        Archive letras = new Archive(pathToFile, CommandLines.selectArraysOrReferences());
+        CommandLines.menu();
+        int selection = Integer.parseInt(CommandLines.input());
+        while (selection<0 || selection>9) {
+            CommandLines.menu();
+            selection = Integer.parseInt(CommandLines.input());
+        }
+        while (selection!=0) {
+            try {
+                switch(selection) {
+                    case 1:
+                        letras.openFile();
+                        letras.setRegex("[U].[\\dA-Z]{4,5}");
+                        letras.readText(dataStructure);
+                        letras.closeFile();
+                        break;
+                    case 2:
+                        letras.removeAll(dataStructure);
+                        break;
+                    case 3:
+                        letras.getDataStructureLength(dataStructure);
+                        break;
+                    case 9:
 
-        try {
-            letras.openFile();
-            letras.setRegex("[U].[\\dA-Z]{4,5}");
-            letras.readText(CommandLines.selectDataStructure());
-            letras.closeFile();
-            
-        } catch (IOException e) {
-            System.err.println("Error: "+e.getMessage());
+                    default:
+                        selection =0;
+                }
+                selection = Integer.parseInt(CommandLines.input());
+                while (selection<0 || selection>9) {
+                    CommandLines.menu();
+                    selection = Integer.parseInt(CommandLines.input());
+                }
+            } catch (IOException e) {
+                System.err.println("Error: "+e.getMessage());
+            }
         }
     }
 }
