feaf7b8c704b9bcbf0a708e06c7491e4bb8f618c
stsanchezl <stsanchezl@unal.edu.co>
2020-10-16 18:09:08 -0500

    Revert "Merge branch 'master' of https://github.com/stsanchezl/sinogramas into master"
    This reverts commit 3d40f9a54c8cdb6be931ecc06f09e42e64347a2c, reversing
    changes made to f298b8ba793efcc00b664437021d6d8789efb603.
    
    Go back to a previous version

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,10 +1,7 @@
 package logic;
-import data.QueueArrayGeneric;
+
 public class Main {
     public static void main(String[] args) {
-        //Runner.runner();
-        QueueArrayGeneric q = new QueueArrayGeneric<>(4);
-        for (int i)
-
+        Runner.runner();
     }
 }
